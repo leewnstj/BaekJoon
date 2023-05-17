c70d661534b7b9520e8de05af6951aad76cfb17a
leewnstj <98932107+leewnstj@users.noreply.github.com>
2023-05-17 14:35:44 +0900

    [level 2] Title: 올바른 괄호, Time: 0.14 ms, Memory: 3.89 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,18 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int n = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (n < 0)
+            return false;
+        if (s[i] == '(')
+            n++;
+        else if (s[i] == ')')
+            n--;
+    }
+    return n == 0;
+}
